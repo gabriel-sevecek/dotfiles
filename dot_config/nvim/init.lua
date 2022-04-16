5ed087d50ad2dd277843a4ead953cb4507845d29
Gabriel Sevecek <g.sevecek@sportradar.com>
2022-04-16 15:47:53 +0200

    neovim: Disabled unused providers

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -67,6 +67,11 @@ vim.g.startify_bookmarks = {{
     z = "~/.zshrc",
 }}
 
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+
 function highlight_on_yank()
     vim.highlight.on_yank { on_visual=false }
 end
