332bb4d1571293b9743c6b3f2c88c4f2d52db81f
Gabriel Sevecek <g.sevecek@sportradar.com>
2022-04-18 13:40:01 +0200

    neovim: Fix nvim-cmp config

--- dot_config/nvim/lua/cmp-setup.lua
+++ dot_config/nvim/lua/cmp-setup.lua
@@ -5,28 +5,15 @@ local set = vim.opt
 set.completeopt = "menu,menuone,noselect"
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            -- For `vsnip` user.
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` user.
-
-            -- For `luasnip` user.
-            -- require('luasnip').lsp_expand(args.body)
-
-            -- For `ultisnips` user.
-            -- vim.fn["vsnip#anonymous"](args.body)
-        end,
-    },
-    mapping = {
+    mapping = cmp.mapping.preset.insert({
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.close(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+    }),
     sources = {
         { name = 'nvim_lsp' },
-        { name = 'vsnip' },
         { name = 'buffer' },
         { name = 'path' },
     }
