dd379ac38caefdf5271ac22dfb3689449726346b
Gabriel Sevecek <1851927+gabriel-sevecek@users.noreply.github.com>
2022-05-05 16:27:55 +0200

    neovim: Remove cursor hold highlighting

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -53,20 +53,6 @@ local on_attach = function(client, bufnr)
         },
       }, { prefix = "<leader>"})
   end
-
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    vim.api.nvim_exec([[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-      augroup lsp_document_highlight
-        autocmd!
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]], false)
-  end
 end
 
 -- Use a loop to conveniently both setup defined servers 
