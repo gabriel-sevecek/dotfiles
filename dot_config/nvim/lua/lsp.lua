62c5f9d7c655e17daf558ba29884e6ebb76c6a61
Gabriel Sevecek <g.sevecek@sportradar.com>
2022-09-19 21:06:14 +0200

    neovim: Remove eslint (in favor of null-ls) and add prismals

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -57,7 +57,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers 
 -- and map buffer local keybindings when the language server attaches
-local servers = { "pyright", "tsserver", "elmls", "hls", "svelte", "clangd", "vuels", "eslint" }
+local servers = { "pyright", "tsserver", "elmls", "hls", "svelte", "clangd", "vuels", "prismals" }
 local init_options = {
     tsserver = {
         preferences = {
