3fd39b2df5a756018f38215d565fcd28d043bbee
Gabriel Sevecek <g.sevecek@sportradar.com>
2022-09-19 21:06:45 +0200

    neovim: Add react snippets for typescript files

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -175,4 +175,4 @@ require"lualine".setup {
     }
 }
 require("luasnip.loaders.from_vscode").lazy_load()
-require"luasnip".filetype_extend("typescript", {"typescript", "javascript"})
+require"luasnip".filetype_extend("typescript", {"typescript", "javascript", "javascriptreact-ts"})
