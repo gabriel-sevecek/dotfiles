ace795e0bb4e777e9a5df249c89c4104eb6c2f55
Gabriel Sevecek <g.sevecek@sportradar.com>
2022-09-19 21:05:31 +0200

    neovim: Remove line numbers from neotree

--- dot_config/nvim/lua/tree.lua
+++ dot_config/nvim/lua/tree.lua
@@ -1,4 +1,15 @@
-require'neo-tree'.setup {}
+require'neo-tree'.setup {
+  event_handlers = {
+    {
+      event = "vim_buffer_enter",
+      handler = function()
+        if vim.bo.filetype == "neo-tree" then
+          vim.cmd("setlocal nonumber")
+        end
+      end,
+    },
+  }
+}
 local wk = require("which-key")
 
 wk.register({
