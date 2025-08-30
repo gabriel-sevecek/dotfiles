83e8296c628e083bc296641000a792ec018977b0
Gabriel Sevecek <g.sevecek@sportradar.com>
2025-08-30 19:37:03 +0200

    (home-manager) Add superhtml (formatter)

--- home.nix
+++ home.nix
@@ -35,29 +35,14 @@
       jq
       nixvim
       nodejs_20
-      pnpm
-      yarn
       pgcli
       pgformatter
+      pnpm
+      podman-compose
       prettierd
       ripgrep
-      podman-compose
-      # # Adds the 'hello' command to your environment. It prints a friendly
-      # # "Hello, world!" when run.
-      # pkgs.hello
-
-      # # It is sometimes useful to fine-tune packages, for example, by applying
-      # # overrides. You can do that directly here, just don't forget the
-      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-      # # fonts?
-      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-      # # You can also create simple shell scripts directly inside your
-      # # configuration. For example, this adds a command 'my-hello' to your
-      # # environment:
-      # (pkgs.writeShellScriptBin "my-hello" ''
-      #   echo "Hello, ${config.home.username}!"
-      # '')
+      superhtml
+      yarn
     ]
     ++ extraPackages;
 
